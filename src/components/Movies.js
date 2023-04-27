d10cfe03f0fbaed76023ff9aa5a4f6779a429fca
danieljuma5 <jumadaniel275@gmail.com>
2023-04-28 00:29:23 +0300

    Completed assignment

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,21 @@
 import React from "react";
 import { movies } from "../data";
-
+console.log(movies)
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+    <h1>Movies Page</h1>
+    {movies.map((movie, index) => (
+  <div key={index}>
+    <h3>Name: {movie.title}</h3>
+    <p>Time: {movie.time}</p>
+        Movies:
+        <ul>
+          {movie.genres.map((genre, index) => (
+            <li key={index}>{genre}</li>
+          ))}
+        </ul>
+  </div>
+))}
+  </div>;
 }
-
 export default Movies;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,45 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+const styleList = {
+  display: "list-item",
+  padding: "0 15px",
+  listStyleType: "disc",
+
+}
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return <div className="navbar">
+    <NavLink
+    to="/"
+    exact="true"
+    style={styleList}
+    >
+      Home
+    </NavLink>
+    <NavLink
+    to="/movies"
+    exact="true"
+    style={styleList}
+    >
+      Movies
+    </NavLink>
+     <NavLink
+    to="/directors"
+    exact="true"
+    style={styleList}
+    >
+      Directors
+    </NavLink>
+     <NavLink
+    to="/actors"
+    exact="true"
+    style={styleList}
+    >
+      Actors
+    </NavLink>
+    
+
+  </div>;
 }
 
 export default NavBar;
